b5c90048b747d805a3dad8214032b6dbebc40a94
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-05-11 04:59:00 +0200

    Update userrecon.sh

--- userrecon.sh
+++ userrecon.sh
@@ -10,15 +10,15 @@ banner() {
 
 
 
-printf "                                                   \e[99;34m.-\"\"\"\"-. \e[0m\n"
-printf "                                                  \e[99;34m/        \ \e[0m\n"
-printf "\e[1;77m  _   _               ____                       \e[0m\e[99;34m/_        _\ \e[0m\n"
-printf "\e[1;77m | | | |___  ___ _ __|  _ \ ___  ___ ___  _ __  \e[0m\e[99;34m// \      / \\ \e[0m\n"
-printf "\e[1;77m | | | / __|/ _ \ '__| |_) / _ \/ __/ _ \| '_ \ \e[0m\e[99;34m|\__\    /__/| \e[0m\n"
-printf "\e[1;77m | |_| \__ \  __/ |  |  _ <  __/ (_| (_) | | | | \e[0m\e[99;34m\    ||    / \e[0m\n"
-printf "\e[1;77m  \___/|___/\___|_|  |_| \_\___|\___\___/|_| |_|  \e[0m\e[99;34m\        / \e[0m\n"
-printf "                   \e[99;34mv1.0, Author: @AL_Alamy      \e[0m   \e[99;34m\  __  / \e[0m\n"
-printf "                                                    \e[99;34m'.__.' \e[0m\n"
+printf "                                                   \e[1;92m.-\"\"\"\"-. \e[0m\n"
+printf "                                                  \e[1;92m/        \ \e[0m\n"
+printf "\e[1;77m  _   _               ____                       \e[0m\e[1;92m/_        _\ \e[0m\n"
+printf "\e[1;77m | | | |___  ___ _ __|  _ \ ___  ___ ___  _ __  \e[0m\e[1;92m// \      / \\ \e[0m\n"
+printf "\e[1;77m | | | / __|/ _ \ '__| |_) / _ \/ __/ _ \| '_ \ \e[0m\e[1;92m|\__\    /__/| \e[0m\n"
+printf "\e[1;77m | |_| \__ \  __/ |  |  _ <  __/ (_| (_) | | | | \e[0m\e[1;92m\    ||    / \e[0m\n"
+printf "\e[1;77m  \___/|___/\___|_|  |_| \_\___|\___\___/|_| |_|  \e[0m\e[1;92m\        / \e[0m\n"
+printf "                   \e[1;92mv1.0, Author: @AL_Alamy      \e[0m   \e[1;92m\  __  / \e[0m\n"
+printf "                                                    \e[1;92m'.__.' \e[0m\n"
                                                 
 
 
@@ -35,7 +35,7 @@ fi
 
 scanner() {
 
-read -p $'\e[99;34m[\e[0m\e[1;77m?\e[0m\e[99;34m] Input Username:\e[0m ' username
+read -p $'\e[1;92m[\e[0m\e[1;77m?\e[0m\e[1;92m] Input Username:\e[0m ' username
 
 if [[ -e $username.txt ]]; then
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Removing previous file:\e[0m\e[1;77m %s.txt" $username
